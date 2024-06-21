7a6b2c10f879c429332c378fa9a30b9a6abba8c6
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2024-06-21 22:45:25 +0530

    Layout+assets added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type { Metadata } from 'next';
 
 import {
   ClerkProvider,
@@ -8,30 +10,61 @@ import {
 } from '@clerk/nextjs'
 import './globals.css'
 import React from 'react'
+
+const inter=Inter({
+  subsets:['latin'],weight:['100','200','300','400','500','600','700','800','900'],variable:'--font-inter'
+})
+
+const spaceGrotesk=Space_Grotesk({
+  subsets:['latin'],weight:['300','400','500','600','700'],variable:'--font-spaceGrotesk'
+})
+ 
+
+
+export const metadata:Metadata={
+    title:"Quanswer",
+    description:"A repositiory of knowledge",
+    keywords:"nextpro1",
+    icons:{
+      icon:'/assets/images/site-logo.svg',
+    }
+}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
+    <html lang="en">
+
+    <ClerkProvider
+    appearance={
+      {
+        elements:{formButtonPrimary:'primary-gradient',footerActionLink:'primary-text-gradient hover:text-primary-500'},
+      }
+    }>
+     
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
 <header>
   <UserButton showName />
 </header>
           <main>
+            <h1  className='h1-bold'>
+              demo text
+            </h1>
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
+             <UserButton showName/>
          
-          {children}
           </SignedIn>
+          {children}
           </main>
          
         </body>
-      </html>
+  
     </ClerkProvider>
+    </html>
   )
 }
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+import {
+    clerkMiddleware,
+    createRouteMatcher
+  } from '@clerk/nextjs/server';
+  
+  const isProtectedRoute = createRouteMatcher([
+    '/ask-question(.*)',
+  
+  ]);
+  
+  export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+  });
+  
+  export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
