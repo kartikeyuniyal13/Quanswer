6f8bd91eecab26193c5b677158063155fb6b999c
Kartikey Uniyal <kartikeyuniyal13@gmail.com>
2025-04-04 01:26:33 +0530

    Add GlobalSearch component to Navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import React from 'react'
 import Theme from './Theme'
 import MobileNav from './MobileNav'
+import GlobalSearch from './GlobalSearch'
 
 
 const Navbar = () => {
@@ -20,7 +21,7 @@ const Navbar = () => {
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden"> <span className="text-primary-500">Quanswer</span></p>
       </Link>
 
-   
+              <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
